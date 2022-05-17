93d5dd01ec6acf1e4e6251d87b3246ce201b303b
vivid90 <vividjm90@gmail.com>
2022-05-17 17:35:51 +0900

    properties > yml로 변경 DB 접속 정보 암호화

---
+++ src/test/java/com/example/demorestapi/configs/JasyptApplicationTests.java
@@ -0,0 +1,38 @@
+package com.example.demorestapi.configs;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class JasyptApplicationTests {
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void jasypt() {
+
+    }
+
+    private String jasyptEncrypt(String input) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(encryptKey);
+        return encryptor.encrypt(input);
+    }
+
+    private String jasyptDecryt(String input){
+        String key = "";
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.decrypt(input);
+    }
+
+}
